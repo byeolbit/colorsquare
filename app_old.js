2fef498435d495c92efc8010a87792b11271e8d8
Byeolbit <info@byeolbit.com>
2016-09-03 16:32:34 +0900

    Remove mongoDB codes

--- app_old.js
+++ app_old.js
@@ -6,10 +6,7 @@ var app = require('express')();
 var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/test';
 
 app.use(express.static('script'));
 app.use(express.static('css'));
@@ -25,61 +22,6 @@ var usedColor = [];
 var stacks = [];
 var scoreList = [];
 
-
-/*
- MongoClient.connect(url, function(err, db) {
- assert.equal(null, err);
- insertDocument(db, function() {
- db.close();
- });
- });
-
- var insertDocument = function(db, callback) {
- db.collection('restaurants').insertOne( {
- "address" : {
- "street" : "2 Avenue",
- "zipcode" : "10075",
- "building" : "1480",
- "coord" : [ -73.9557413, 40.7720266 ]
- },
- "borough" : "Manhattan",
- "cuisine" : "Italian",
- "grades" : [
- {
- "date" : new Date("2014-10-01T00:00:00Z"),
- "grade" : "A",
- "score" : 11
- },
- {
- "date" : new Date("2014-01-16T00:00:00Z"),
- "grade" : "B",
- "score" : 17
- }
- ],
- "name" : "Vella",
- "restaurant_id" : "41704620"
- }, function(err, result) {
- assert.equal(err, null);
- console.log("Inserted a document into the restaurants collection.");
- callback();
- });
- };
- */
-/*
- function writeSave(){
- var txtFile = "save.txt";
- var file = new File(txtFile);
-
- file.open("w");
- file.writeln(teamList);
- file.writeln(cellInfo);
- file.writeln(colorList);
- file.writeln(userLists);
- file.writeln(teamCount);
- file.writeln(clients);
-
- }
- */
 function celldata(c, s, t){
     this.color = c;
     this.stack = s;
@@ -87,17 +29,6 @@ function celldata(c, s, t){
     this.isCompleted = false;
 }
 
-/*
- function client_class(ID, addr){
- this.id = ID;
- this.team = null;
- this.tname = null;
- this.nick = newNick();
- this.point = 0;
- this.ping = true;
- this.address = addr;
- }
- */
 function client_class(ID){
     this.id = ID;
     this.team = null;
